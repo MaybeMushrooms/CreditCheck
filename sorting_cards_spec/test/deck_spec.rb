abdb7b798f65a474ac3d72c0a3858520208f7890
maybemushrooms <thecraftedgem@tuta.io>
2018-08-21 11:52:08 -0600

    added spec folder

--- sorting_cards_spec/test/deck_spec.rb
+++ sorting_cards_spec/test/deck_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Deck do
     expect(deck.cards).to eq [card_1, card_2, card_3]
     expect(deck.count).to eq 3
   end
+
+  it "sorts deck" do
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("Jack", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    card_5 = Card.new("Ace", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+    expect(deck.sort).to eq  [card_1, card_3, card_2, card_5, card_4]
+  end
 end
